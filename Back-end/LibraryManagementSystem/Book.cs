9fb3ad1c9a239600dc34a5cb5d5d0b2d92a3ae50
Gagana Hansika Sonnadara <gaganahsonnadara99@gmail.com>
2024-12-27 18:22:07 +0530

    Uploaded Backend files

---
+++ Back-end/LibraryManagementSystem/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagementSystem
+{
+    public class Book
+    {
+        //Id, Title, Author, Description
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public  string Author { get; set; } = string.Empty ;
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
---
+++ Back-end/LibraryManagementSystem/Controllers/BookController.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.InteropServices;
+using LibraryManagementSystem.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BookController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Book>>> AddBook(Book book) 
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetAllBooks() 
+        {
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id) 
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) 
+            {
+                return BadRequest("Book not Found..!!");
+            }
+            return Ok(book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Book>>> UpdateBook(int id, Book updatedBook)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return BadRequest("Book not Found..!!");
+            }
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Description = updatedBook.Description;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Book>>> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return BadRequest("Book not Found..!!");
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+
+
+    }
+}
